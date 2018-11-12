bb721590141525ad1d9fb8a0857cce884434bf91
Danilo Cândido <danilod07@gmail.com>
2018-11-12 15:25:48 -0300

    add pg gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,7 +34,7 @@ end
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Requests::JsonHelpers
-  
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
